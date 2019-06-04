93a8f521ffd6549fa1e9d09a1df597ed31b01f1e
yurseria <yurseria@gmail.com>
2019-06-05 01:09:26 +0900

    rename package to simpmonit

--- src/report.js
+++ src/report.js
@@ -1,30 +1,39 @@
-const fs = require('fs');
-const _ = require('lodash');
-const prettyBytes = require('pretty-bytes');
+const fs = require("fs");
+const _ = require("lodash");
+const prettyBytes = require("pretty-bytes");
 const path = require("path");
 const dayjs = require("dayjs");
-const reportDir = path.join(process.cwd(), "./reports");
+const reportDir = path.join(process.cwd(), "./simpmonit-reports");
 const dateFileFormat = "YYYYMMDD";
 const dateTimeFormat = "YYYY-MM-DD HH:mm:ss.SSS";
 
 module.exports.check = _ => {
   fs.existsSync(reportDir) || fs.mkdirSync(reportDir);
-}
+};
 
 module.exports.reportToFile = stats => {
   _.forEach(stats, (value, key) => {
     fs.writeFileSync(
-      path.join(reportDir, `reports_${key}.${dayjs().format(dateFileFormat)}`),
-      `${dayjs(value.timestamp).format(dateTimeFormat)}, CPU : ${value.cpu}%, MEM : ${prettyBytes(value.memory)}\n`,
+      path.join(
+        reportDir,
+        `reports_pid${key}_${dayjs().format(dateFileFormat)}.log`
+      ),
+      `${dayjs(value.timestamp).format(dateTimeFormat)}, CPU : ${
+        value.cpu
+      }%, MEM : ${prettyBytes(value.memory)}\n`,
       {
         flag: "a"
       }
     );
   });
-}
+};
 
 module.exports.reportToConsole = stats => {
   _.forEach(stats, (value, key) => {
-    console.log(`[${key}] ${dayjs(value.timestamp).format(dateTimeFormat)}, CPU : ${value.cpu}%, MEM : ${prettyBytes(value.memory)}`);
+    console.log(
+      `[${key}] ${dayjs(value.timestamp).format(dateTimeFormat)}, CPU : ${
+        value.cpu
+      }%, MEM : ${prettyBytes(value.memory)}`
+    );
   });
-}
+};
